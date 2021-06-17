2a730aa9af1f1c4ef85bb724524db0bbd929d459
PeterStark000 <40671520+PeterStark000@users.noreply.github.com>
2021-06-17 18:58:27 -0300

    Update drawCenteredImageInRect.js

--- src/utils/drawCenteredImageInRect.js
+++ src/utils/drawCenteredImageInRect.js
@@ -1,12 +1,12 @@
 const { drawImage } = require('canvas');
 
-module.exports = async function drawCenteredImageInRect = function(context, image, startX, startY, scale) {
-
-var imageHeight = getScale(image.height, image.width).height
-var imageWidth = getScale(image.height, image.width).width
-
-var imageStartX = (startX + (scale/2)) - (imageHeight/2);
-var imageStartY = (startY + (scale/2)) - (imageWidth/2);
+module.exports = async function drawCenteredImageInRect(context, image, startX, startY, scale) {
+  
+  var imageHeight = getScale(image.height, image.width).height
+  var imageWidth = getScale(image.height, image.width).width
+  
+  var imageStartX = (startX + (scale/2)) - (imageHeight/2);
+  var imageStartY = (startY + (scale/2)) - (imageWidth/2);
 
   drawImage(image, imageStartX, imageStartY, imageHeight, imageWidth);
   
